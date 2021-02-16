921e05c9a87aa922b2cac2a560085e41d0ed63b8
Shivam Mishra <s.mishra0722@gmail.com>
2021-02-16 20:54:23 +0530

    mongodb user

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,31 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
-import postInputs from './routes/inputs.js';
+import postInputs from "./routes/inputs.js";
 
 const app = express(); //Initializing app
 
-
-
 //Setting up bodyparser to send request properly
-app.use(bodyParser.json({limit: '30mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/inputs', postInputs);
+app.use("/inputs", postInputs);
+
+const uname = "smishra0722";
+const pass = "smishra0722";
 
 //Connecting application with database
-const CONNECTION_URL = 'mongodb+srv://smishra0722:smishra0722@cluster0.mgzbk.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const CONNECTION_URL = `mongodb+srv://${uname}:${pass}@cluster0.mgzbk.mongodb.net/<dbname>?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology:true}) //Object To make sure we dont get any warning in console
-.then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
-.catch((error) => console.log(error.message));
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }) //Object To make sure we dont get any warning in console
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModify', false); //To make sure we dont get any warning in console
-
-
+mongoose.set("useFindAndModify", false); //To make sure we dont get any warning in console
